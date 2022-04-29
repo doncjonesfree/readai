9f07130c51697c5d438b6f11d102cefb72bca32a
Don Jones <doncjones1@gmail.com>
2022-04-28 17:11:28 -0700

    added user accounts

--- README.txt
+++ README.txt
@@ -6,9 +6,13 @@ https://blaze-tutorial.meteor.com/simple-todos/03-forms-and-events.html
 https://blaze-tutorial.meteor.com/simple-todos/04-update-and-remove.html
 https://blaze-tutorial.meteor.com/simple-todos/05-styles.html
 https://blaze-tutorial.meteor.com/simple-todos/06-filter-tasks.html
-https://blaze-tutorial.meteor.com/simple-todos/07-adding-user-accounts.html -- next to do
+https://blaze-tutorial.meteor.com/simple-todos/07-adding-user-accounts.html
+https://blaze-tutorial.meteor.com/simple-todos/08-methods.html
 
 */
 
 meteor add dev-error-overlay
 meteor add reactive-dict
+meteor add accounts-password
+meteor npm install --save bcrypt
+meteor remove insecure
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,24 +1,48 @@
 import { Template } from 'meteor/templating';
-import { TasksCollection } from "../api/TasksCollection";
+import { TasksCollection } from '/imports/db/TasksCollection';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './App.html';
 import './Task.js';
+import "./Login.js";
 
 const HIDE_COMPLETED_STRING = "hideCompleted";
+
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
 
 Template.mainContainer.onCreated(function mainContainerOnCreated() {
   this.state = new ReactiveDict();
 });
+
+const getTasksFilter = () => {
+  const user = getUser();
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  return { userFilter, pendingOnlyFilter };
+}
 
 Template.mainContainer.helpers({
   tasks() {
     const instance = Template.instance();
     const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
 
-    const hideCompletedFilter = { isChecked: { $ne: true } };
+    const { pendingOnlyFilter, userFilter } = getTasksFilter();
+    console.log('jones36 pendingOnlyFilter',pendingOnlyFilter);
+    console.log('jones36 userFilter',userFilter);
+    console.log('jones36 hideCompleted',hideCompleted);
+    console.log('jones36 hideCompleted ? pendingOnlyFilter : userFilter',hideCompleted ? pendingOnlyFilter : userFilter);
 
-    return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+    if (!isUserLogged()) {
+     return [];
+    }
+
+    return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
       sort: { createdAt: -1 },
     }).fetch();
   },
@@ -26,15 +50,31 @@ Template.mainContainer.helpers({
     return Template.instance().state.get(HIDE_COMPLETED_STRING);
   },
   incompleteCount() {
-    const incompleteTasksCount = TasksCollection.find({ isChecked: { $ne: true } }).count();
+    if (!isUserLogged()) {
+      return '';
+    }
+    const { pendingOnlyFilter } = getTasksFilter();
+
+    const incompleteTasksCount = TasksCollection.find(pendingOnlyFilter).count();
     return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
   },
+  isUserLogged() {
+    const tmp = isUserLogged();
+    console.log('jones38',Meteor.user());
+    return tmp;
+  },
+  getUser() {
+    return getUser();
+  }
 });
 
 Template.mainContainer.events({
   "click #hide-completed-button"(event, instance) {
     const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
     instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  },
+  'click .user'() {
+    Meteor.logout();
   }
 });
 
@@ -48,10 +88,7 @@ Template.form.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    TasksCollection.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
