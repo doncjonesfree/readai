e9869b6b3c81efd5649c7b01dee52086d77f0d7d
Don Jones <doncjones1@gmail.com>
2022-05-14 14:13:19 -0700

    startup: collections created for gather facts, draw conclusions and power builder

--- imports/db/Collections.js
+++ imports/db/Collections.js
@@ -2,3 +2,6 @@ import { Mongo } from 'meteor/mongo';
 
 export const TasksCollection = new Mongo.Collection('tasks');
 export const GatherFacts = new Mongo.Collection('gather_facts');
+export const GatherFactsAnswers = new Mongo.Collection('gather_facts_answers');
+export const DrawConclusions = new Mongo.Collection('draw_conclusions');
+export const PowerBuilder = new Mongo.Collection('power_builder');
--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { TasksCollection, GatherFacts } from '/imports/db/Collections';
+import { TasksCollection, GatherFacts, GatherFactsAnswers, DrawConclusions, PowerBuilder } from '/imports/db/Collections';
 import '/imports/api/tasksMethods';
 import '/imports/api/tasksPublications';
 
@@ -40,8 +40,209 @@ Meteor.startup(() => {
   }
 
   loadGatherFacts();
+  loadGatherFactsAnswers();
+  loadDrawConclusions();
+  loadPowerBuilder();
 
 });
+
+
+const loadPowerBuilder = function(){
+  const process = function(v){
+    if ( v && lib.verifyInteger(v) ) return lib.int(v);
+    if ( v && lib.verifyFloat(v) ) return lib.float(v);
+    if ( typeof(v) === 'string') v = v.trim();
+    return v;
+  };
+
+  const getNbr = function(s){
+    const ix = s.indexOf('~');
+    if ( ix < 0 ) return 0;
+    const t = s.substring(ix+1).split(' ');
+    return lib.int(t[0]);
+  };
+
+  // Headings:
+  // 'LessonNum', 'Code', 'Color', 'Number', 'GradeLevel', 'Title','Article',
+  // 'HowWell', 'LearnWords', 'Answers','CB', 'CDT','MB', 'MDT'
+
+  if ( ! PowerBuilder.find().count() ) {
+    const fullPath = Assets.absoluteFilePath('txt/powerb.txt' );
+    let contents = fs.readFileSync(fullPath, 'binary').split('\n');
+    let headings = contents[0].split('\t');
+    let first = true; // first line in a new record
+    let obj = {};
+    let field = '';
+    let nbr = 0;
+    let list = [];
+    const sequence = ['Article','HowWell','LearnWords','Answers'];
+    for ( let i=1; i < contents.length; i++ ) {
+      const raw = contents[i].replace(/\r/g,'');
+      const line = raw.split('\t');
+      if ( raw.indexOf('\t') >= 0 && lib.verifyInteger(line[0] )) {
+        if ( ! first ) {
+          list.push(obj);
+        }
+        obj = {};
+        obj.lesson = [];
+        for ( let i2=0; i2 <= 6; i2++ ) {
+          if ( i2 === 6 ) {
+            obj.lesson.push(line[i2]);
+          } else {
+            obj[ headings[i2] ] = process( line[i2] );
+          }
+        }
+        first = false;
+      } else {
+        obj.lesson.push(raw);
+      }
+    }
+    for ( let i=0; i < list.length; i++ ) {
+      PowerBuilder.insert(list[i]);
+    }
+    console.log('PowerBuilder loaded, %s lessons',list.length);
+  }
+};
+
+const loadDrawConclusions = function(){
+  if ( ! DrawConclusions.find().count() ) {
+    const list = getDataFromTxt( 'drawconclusions.txt' );
+    for ( let i=0; i < list.length; i++ ) {
+      DrawConclusions.insert(list[i]);
+    }
+    console.log('DrawConclusions loaded, %s lessons',list.length);
+  }
+};
+
+const getDataFromTxt = function( fileName ){
+  // return a list of records to be inserted in a collection
+
+  let list = []; // list of records
+
+  const makeObject = function(headings,line){
+    let obj = {};
+    const ignore = ['CB','CDT','MB','MDT'];
+    let newRec = false;
+    if ( lib.verifyInteger(line[0] )) newRec = true;
+    if ( newRec ) {
+      for ( let i=0; i < headings.length; i++ ) {
+        const h = headings[i];
+        let v = line[i];
+        if ( typeof(v) === 'string') v = v.trim();
+        if ( lib.verifyInteger(v) ) v = lib.int(v);
+        if ( lib.verifyFloat(v) ) v = lib.float(v);
+        if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
+      }
+      list.push(obj);
+    } else {
+      // add to previous value
+      const ix = list.length - 1;
+      if ( ix < 0 ) console.log('Error in line 74 main.js');
+      if ( ix >= 0 ) {
+        obj = list[ix];
+        let start = 5;
+        for ( let i=start; i < headings.length; i++ ) {
+          const h = headings[i];
+          let v = line[i-start];
+          if ( typeof(v) === 'string') v = v.trim();
+          if ( lib.verifyInteger(v) ) v = lib.int(v);
+          if ( lib.verifyFloat(v) ) v = lib.float(v);
+          if ( v ) {
+            if ( i === start ) {
+              obj.Paragraph += v;
+            } else {
+              if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
+            }
+          }
+        }
+        list[ix] = obj;
+      }
+    }
+    return obj;
+  };
+
+  const fullPath = Assets.absoluteFilePath('txt/' + fileName );
+
+  let contents = fs.readFileSync(fullPath, 'binary').split('\n');
+  let headings = [];
+  for ( let i=0; i < contents.length; i++ ) {
+    const raw = contents[i].replace(/\r/g,'');
+    const line = raw.split('\t');
+    if ( i === 0 ) {
+      headings = line;
+    } else {
+      makeObject(headings,line); // adds to list array
+    }
+  }
+  return list;
+};
+
+const loadGatherFactsAnswers = function(){
+  // If not defined - create gather facts collection
+
+  let list = []; // list of records
+
+  const makeObject = function(headings,line){
+    let obj = {};
+    const ignore = ['CB','CDT','MB','MDT'];
+    if ( lib.verifyInteger(line[0] )) {
+      for ( let i=0; i < headings.length; i++ ) {
+        const h = headings[i];
+        let v = line[i];
+        if ( typeof(v) === 'string') v = v.trim();
+        if ( lib.verifyInteger(v) ) v = lib.int(v);
+        if ( lib.verifyFloat(v) ) v = lib.float(v);
+        if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
+      }
+      list.push(obj);
+    } else {
+      // add to previous value
+      const ix = list.length - 1;
+      if ( ix < 0 ) console.log('Error in line 134 main.js');
+      if ( ix >= 0 ) {
+        obj = list[ix];
+        for ( let i=5; i < headings.length; i++ ) {
+          const h = headings[i];
+          let v = line[i-5];
+          if ( typeof(v) === 'string') v = v.trim();
+          if ( lib.verifyInteger(v) ) v = lib.int(v);
+          if ( lib.verifyFloat(v) ) v = lib.float(v);
+          if ( v ) {
+            if ( i === 5 ) {
+              obj.Paragraph += v;
+            } else {
+              if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
+            }
+          }
+        }
+        list[ix] = obj;
+      }
+    }
+    return obj;
+  };
+
+  if ( ! GatherFactsAnswers.find().count() ) {
+    // const fullPath = Assets.absoluteFilePath('rpt/gatherfacts.rpt');
+    const fullPath = Assets.absoluteFilePath('txt/gatherfactsanswers.txt');
+
+    let contents = fs.readFileSync(fullPath, 'binary').split('\n');
+    let headings = [];
+    for ( let i=0; i < contents.length; i++ ) {
+      const raw = contents[i];
+      const line = raw.split('\t');
+      if ( i === 0 ) {
+        headings = line;
+      } else {
+        makeObject(headings,line); // adds to list array
+      }
+    }
+    for ( let i=0; i < list.length; i++ ) {
+      GatherFactsAnswers.insert(list[i]);
+    }
+    console.log('GatherFactsAnswers loaded, %s lessons',list.length);
+  }
+};
+
 
 const loadGatherFacts = function(){
   // If not defined - create gather facts collection
@@ -50,17 +251,21 @@ const loadGatherFacts = function(){
 
   const makeObject = function(headings,line){
     let obj = {};
+    const ignore = ['CB','CDT','MB','MDT'];
     if ( lib.verifyInteger(line[0] )) {
       for ( let i=0; i < headings.length; i++ ) {
         const h = headings[i];
-        const v = line[i];
-        if ( v ) obj[h] = v.trim();
+        let v = line[i];
+        if ( typeof(v) === 'string') v = v.trim();
+        if ( lib.verifyInteger(v) ) v = lib.int(v);
+        if ( lib.verifyFloat(v) ) v = lib.float(v);
+        if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
       }
       list.push(obj);
     } else {
       // add to previous value
       const ix = list.length - 1;
-      if ( ix < 0 ) console.log('Error in line 63 main.js');
+      if ( ix < 0 ) console.log('Error in line 201 main.js');
       if ( ix >= 0 ) {
         obj = list[ix];
         for ( let i=5; i < headings.length; i++ ) {
@@ -68,11 +273,12 @@ const loadGatherFacts = function(){
           let v = line[i-5];
           if ( typeof(v) === 'string') v = v.trim();
           if ( lib.verifyInteger(v) ) v = lib.int(v);
+          if ( lib.verifyFloat(v) ) v = lib.float(v);
           if ( v ) {
             if ( i === 5 ) {
               obj.Paragraph += v;
             } else {
-              if ( v ) obj[h] = v;
+              if ( v && ignore.indexOf(h) < 0 ) obj[h] = v;
             }
           }
         }
@@ -84,23 +290,22 @@ const loadGatherFacts = function(){
 
   if ( ! GatherFacts.find().count() ) {
     // const fullPath = Assets.absoluteFilePath('rpt/gatherfacts.rpt');
-    const fullPath = Assets.absoluteFilePath('rpt/gftest2.txt');
+    const fullPath = Assets.absoluteFilePath('txt/gatherfacts.txt');
 
     let contents = fs.readFileSync(fullPath, 'binary').split('\n');
     let headings = [];
     for ( let i=0; i < contents.length; i++ ) {
       const raw = contents[i];
-      console.log('jones44 ------------------------------------');
-      console.log('jones44a i=%s raw=%s',i,raw);
       const line = raw.split('\t');
       if ( i === 0 ) {
         headings = line;
-        console.log('jones44b headings',headings);
       } else {
-        console.log('jones44c',makeObject(headings,line));
+        makeObject(headings,line); // adds to list array
       }
-      if ( i > 10 ) break;
     }
-    console.log('jones100',list);
+    for ( let i=0; i < list.length; i++ ) {
+      GatherFacts.insert(list[i]);
+    }
+    console.log('GatherFacts loaded, %s lessons',list.length);
   }
 };
